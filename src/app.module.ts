41b096ffb6553404ca0734e9ea8565c6ee3cfaf5
Valeriia Zvezdina <valeriia.zvezdina@gmail.com>
2024-10-28 14:07:23 +0100

    trivial: remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FlowersModule } from './flowers/flowers.module';
 
 @Module({
-  imports: [FlowersModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [FlowersModule],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
