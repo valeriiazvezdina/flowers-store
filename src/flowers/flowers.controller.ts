4541a022b763941f5271e37a3eb5ba2808a677ba
Valeriia Zvezdina <valeriia.zvezdina@gmail.com>
2024-10-28 14:55:23 +0100

    feat: add guard and pipe

--- src/flowers/flowers.controller.ts
+++ src/flowers/flowers.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/conception/guard';
+import { ParseIntPipe } from 'src/conception/pipe';
 import { FlowersService } from './flowers.service';
 
 @Controller('flowers')
@@ -6,7 +8,14 @@ export class FlowersController {
 	constructor(private readonly flowersService: FlowersService) {}
 
 	@Get('')
+	@UseGuards(AuthGuard)
 	findAll() {
+		return this.flowersService.findAll();
+	}
+
+	@Get('pipe')
+	findAllWithQuery(@Query('pageNumber', ParseIntPipe) pageNumber: number) {
+		console.log('Query:', pageNumber);
 		return this.flowersService.findAll();
 	}
 }
