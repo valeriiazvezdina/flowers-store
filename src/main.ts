cffe07fc90aa52427e334897c24f94659905dcaa
Valeriia Zvezdina <valeriia.zvezdina@gmail.com>
2024-11-05 11:23:45 +0100

    trivial: add nestjs/microservices

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	app.setGlobalPrefix('api');
+	const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+		AppModule,
+		{
+			transport: Transport.TCP,
+		}
+	);
 	app.useGlobalPipes(new ValidationPipe());
-	await app.listen(3000);
 }
 bootstrap();
